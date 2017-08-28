11264b5972ae91e223f6fa6d88233b5003ad8362
Thomas Henderson <thenderson@8thlight.com>
2017-08-28 17:06:28 -0500

    Watch board not game (WARNING: Should watch game's board?)

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -4,10 +4,11 @@ require "tic_tac_toe"
 describe Display do
   before(:each) do
     @game = Game.new
+    @board = Board.new
     @player1 = Player.new("Player 1", "X")
     @player2 = Player.new("Player 2", "O")
     @game.start(@player1, @player2)
-    @display = Display.new(@game)
+    @display = Display.new(@board)
   end
   it "empty boards look right" do
     board = @display.view

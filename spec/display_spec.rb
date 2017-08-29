15cbec261a9f408c0df39ab2ca50bc67e743e307
Thomas Henderson <thenderson@8thlight.com>
2017-08-29 14:52:27 -0500

    Helpful display tests (thanks Sara!)

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -19,4 +19,36 @@ describe Display do
     @game.accept_move(@player1, Move.new([0, 0]))
     expect(@display.view).to include(" X |   |   ")
   end
+  describe 'board_cells' do
+    it 'adds a move to the cell array' do
+      moves = Hash.new(:no_one)
+      move = Move.new(0,0)
+      moves[move] = @player1
+      cells = @display.board_cells(moves)
+      expect(cells[0]).to eql(' X ')
+    end
+  end
+  describe 'draw_board' do
+    it 'will draw an board with a move in it' do
+      moves = Hash.new(:no_one)
+      move = Move.new(0,1)
+      moves[move] = @player1
+      expect(@display.draw_board(moves)).to include(" X |   |   ")
+    end
+  end
+  describe 'update' do
+    it 'empty boards have empty rows when updated' do
+      moves = Hash.new(:no_one)
+      @display.update(moves)
+      expect(@display.view).to include("   |   |   \n")
+    end
+    it 'draws the right update given a collection of moves' do
+      moves = Hash.new(:no_one)
+      move = Move.new(0,1)
+      moves[move] = @player1
+      @display.update(moves)
+      expect(@display.view).to include("   | X |   ")
+    end
+  end
 end
+

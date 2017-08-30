7edc78f79960d797c7396f33a8cafb12f7de2714
Thomas Henderson <thenderson@8thlight.com>
2017-08-30 14:00:33 -0500

    Test board differently?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,15 +6,32 @@ require "move"
 describe Board do
   context "rows, columns, and diagonals" do
     before(:each) do
-      @player = Player.new
-      @another_player = Player.new
+      @player = DeterministicPlayer.new(1)
+      @another_player = DeterministicPlayer.new(2)
       @game = Game.new(@player, @another_player)
       @board = @game.board
     end
-    it "rows begin empty" do
-      expect(@board.row(0)).to be_empty
-      expect(@board.row(1)).to be_empty
-      expect(@board.row(2)).to be_empty
+    context "emptiness" do
+      it "rows begin empty" do
+        expect(@board.row(0)).to be_empty
+        expect(@board.row(1)).to be_empty
+        expect(@board.row(2)).to be_empty
+      end
+      it "columns begin empty" do
+        expect(@board.column(0)).to be_empty
+        expect(@board.column(1)).to be_empty
+        expect(@board.column(2)).to be_empty
+      end
+      it "pos_diagonal begins empty" do
+        expect(@board.pos_diagonal).to be_empty
+        expect(@board.pos_diagonal).to be_empty
+        expect(@board.pos_diagonal).to be_empty
+      end
+      it "neg_diagonal begins empty" do
+        expect(@board.neg_diagonal).to be_empty
+        expect(@board.neg_diagonal).to be_empty
+        expect(@board.neg_diagonal).to be_empty
+      end
     end
     it "rows can be filled" do
       @game.accept_move(@player, Move.new([0,0]))
@@ -28,11 +45,6 @@ describe Board do
       @game.accept_move(@player, Move.new([0,2]))
       expect(@board.row(0).size).to be < 3
     end
-    it "columns begin empty" do
-      expect(@board.column(0)).to be_empty
-      expect(@board.column(1)).to be_empty
-      expect(@board.column(2)).to be_empty
-    end
     it "columns can be filled" do
       @game.accept_move(@player, Move.new([0,0]))
       @game.accept_move(@player, Move.new([1,0]))
@@ -45,11 +57,6 @@ describe Board do
       @game.accept_move(@player, Move.new([2,0]))
       expect(@board.column(0).size).to be < 3
     end
-    it "pos_diagonal begins empty" do
-      expect(@board.pos_diagonal).to be_empty
-      expect(@board.pos_diagonal).to be_empty
-      expect(@board.pos_diagonal).to be_empty
-    end
     it "pos_diagonal can be filled" do
       @game.accept_move(@player, Move.new([0,0]))
       @game.accept_move(@player, Move.new([1,1]))
@@ -61,11 +68,6 @@ describe Board do
       @game.accept_move(@player, Move.new([1,2]))
       @game.accept_move(@player, Move.new([2,2]))
       expect(@board.pos_diagonal.size).to be < 3
-    end
-    it "neg_diagonal begins empty" do
-      expect(@board.neg_diagonal).to be_empty
-      expect(@board.neg_diagonal).to be_empty
-      expect(@board.neg_diagonal).to be_empty
     end
     it "neg_diagonal can be filled" do
       @game.accept_move(@player, Move.new([0,2]))
